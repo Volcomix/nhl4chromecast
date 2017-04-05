2270993c327a785a48177c4752b1bdd1f9dde11b
Volcomix <volcomix@gmail.com>
2017-04-06 00:08:06 +0200

    Remove custom header from video view

--- containers/Video.js
+++ containers/Video.js
@@ -1,60 +1,28 @@
 import React, { Component } from 'react'
 import {
   View,
-  TouchableOpacity,
   Text,
-  Image,
   StyleSheet
 } from 'react-native'
-import { connect } from 'react-redux'
-import { NavigationActions } from 'react-navigation'
-
-import { keyColor, headerHeight } from '../constants/styles'
 
 class Video extends Component {
+  static navigationOptions = {
+    title: 'Vidéo',
+  }
+
   render() {
     const { params: { game, media } } = this.props.navigation.state
     const { teams: { away: { team: away }, home: { team: home } } } = game
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.appBar}>
-            <TouchableOpacity
-              style={styles.leftIcon}
-              onPress={this.goBack}
-            >
-              <View style={styles.leftButton}>
-                <Image
-                  style={styles.icon}
-                  source={require('../img/left.png')}
-                />
-                <Text style={[
-                  styles.text,
-                  styles.button,
-                  styles.leftButtonText
-                ]}>
-                  Retour
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <Text style={[styles.text, styles.title]}>Vidéo</Text>
-          </View>
-        </View>
-        <View style={styles.content}>
-          <Text>
-            {away.name} - {home.name}
-          </Text>
-          <Text>
-            {this.formatFeed(media)}
-          </Text>
-        </View>
+        <Text>
+          {away.name} - {home.name}
+        </Text>
+        <Text>
+          {this.formatFeed(media)}
+        </Text>
       </View>
     )
-  }
-
-  goBack = () => {
-    const { dispatch } = this.props
-    dispatch(NavigationActions.back())
   }
 
   formatFeed = ({ feedName, mediaFeedType, callLetters }) => {
@@ -73,54 +41,9 @@ class Video extends Component {
 
 const styles = StyleSheet.create({
   container: {
-  },
-  header: {
-    backgroundColor: 'rgba(247, 247, 247, 0.96)',
-    borderBottomWidth: 0.5,
-    borderBottomColor: 'rgba(0, 0, 0, 0.3)',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 10,
-  },
-  appBar: {
-    height: headerHeight,
-    paddingTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 17,
-  },
-  title: {
-    fontWeight: '600',
-  },
-  button: {
-    color: keyColor,
-  },
-  leftButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  leftButtonText: {
-    paddingLeft: 7,
-  },
-  leftIcon: {
-    position: 'absolute',
-    left: 8,
-    paddingTop: 20,
-  },
-  icon: {
-    width: 13,
-    height: 21,
-    resizeMode: 'contain',
-  },
-  content: {
-    paddingTop: headerHeight + 8,
+    paddingTop: 8,
     alignItems: 'center',
   },
 })
 
-export default connect()(Video)
+export default Video
