2b47901dd8f35e514d74343fdb1d019db12ed77c
Volcomix <volcomix@gmail.com>
2017-04-17 23:28:44 +0200

    Show dialog to choose game media feed

--- components/GameRow.js
+++ components/GameRow.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
 import { ListItem } from 'material-ui/List'
 
-const GameRow = ({ game, onGamePressed }) => {
-  const away = game.teams.away.team
-  const home = game.teams.home.team
-  return (
-    <ListItem
-      style={styles.container}
-      innerDivStyle={styles.listItem}
-      leftIcon={
-        <img
-          style={styles.image}
-          src={`img/${away.abbreviation.toLowerCase()}.png`}
-        />
-      }
-      rightIcon={
-        <img
-          style={styles.image}
-          src={`img/${home.abbreviation.toLowerCase()}.png`}
-        />
-      }
-      onTouchTap={onGamePressed}
-    >
-      <div style={styles.teamNames}>
-        <span>{away.teamName}</span>
-        <span>{home.teamName}</span>
-      </div>
-    </ListItem>
-  )
+class GameRow extends Component {
+  render() {
+    const { game } = this.props
+    const away = game.teams.away.team
+    const home = game.teams.home.team
+    return (
+      <ListItem
+        style={styles.container}
+        innerDivStyle={styles.listItem}
+        leftIcon={
+          <img
+            style={styles.image}
+            src={`img/${away.abbreviation.toLowerCase()}.png`}
+          />
+        }
+        rightIcon={
+          <img
+            style={styles.image}
+            src={`img/${home.abbreviation.toLowerCase()}.png`}
+          />
+        }
+        onTouchTap={this.handleGamePressed}
+      >
+        <div style={styles.teamNames}>
+          <span>{away.teamName}</span>
+          <span>{home.teamName}</span>
+        </div>
+      </ListItem>
+    )
+  }
+
+  handleGamePressed = () => {
+    const { game, onGamePressed } = this.props
+    onGamePressed(game)
+  }
 }
 
 const styles = {
--- components/Games.js
+++ components/Games.js
@@ -1,25 +1,84 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CircularProgress from 'material-ui/CircularProgress'
-import { List } from 'material-ui/List'
+import { List, ListItem } from 'material-ui/List'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
 
 import GameRow from './GameRow'
 
-const Games = ({ isFetching, games, onGamePressed }) => (
-  <div style={styles.container}>
-    {isFetching ?
-      <CircularProgress /> :
-      <List style={styles.list}>
-        {games.map(game => (
-          <GameRow
-            key={game.gamePk}
-            game={game}
-            onGamePressed={onGamePressed}
-          />
-        ))}
-      </List>
+class Games extends Component {
+  state = {
+    isDialogOpen: false,
+    game: undefined,
+  }
+
+  render() {
+    const { isFetching, games, onGamePressed } = this.props
+    const { game } = this.state
+    let title, media
+    if (game !== undefined) {
+      const away = game.teams.away.team
+      const home = game.teams.home.team
+      title = `${away.teamName} @ ${home.teamName}`
+      media = game.content.media.epg.find(media => media.title === 'NHLTV')
     }
-  </div>
-)
+    return (
+      <div style={styles.container}>
+        {isFetching ?
+          <CircularProgress /> :
+          <List style={styles.list}>
+            {games.map(game => (
+              <GameRow
+                key={game.gamePk}
+                game={game}
+                onGamePressed={this.handleGamePressed}
+              />
+            ))}
+          </List>
+        }
+        <Dialog
+          title={title}
+          contentStyle={styles.dialog}
+          actionsContainerStyle={styles.actions}
+          actions={[<FlatButton
+            label="Annuler"
+            primary={true}
+            onTouchTap={this.handleDialogClosed}
+          />]}
+          open={this.state.isDialogOpen}
+          onRequestClose={this.handleDialogClosed}
+        >
+          <List>
+            {media && media.items.map(feed => (
+              <ListItem key={feed.guid} primaryText={this.formatFeed(feed)} />
+            ))}
+          </List>
+        </Dialog>
+      </div>
+    )
+  }
+
+  handleGamePressed = game => {
+    this.setState({ isDialogOpen: true, game })
+  }
+
+  handleDialogClosed = () => {
+    this.setState({ isDialogOpen: false })
+  }
+
+  formatFeed = ({ feedName, mediaFeedType, callLetters }) => {
+    if (feedName) {
+      return feedName
+    } else {
+      let name = mediaFeedType[0] + mediaFeedType.slice(1).toLowerCase()
+      if (callLetters) {
+        return `${name} (${callLetters})`
+      } else {
+        return name
+      }
+    }
+  }
+}
 
 const styles = {
   container: {
@@ -37,6 +96,13 @@ const styles = {
     flex: 1,
     alignSelf: 'flex-start',
     maxWidth: 400,
+  },
+  dialog: {
+    maxWidth: 400,
+    textAlign: 'center',
+  },
+  actions: {
+    textAlign: 'center',
   },
 }
 
