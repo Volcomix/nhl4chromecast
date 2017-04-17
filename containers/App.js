fa74566e9b28990c3a30db59188e6e886935f9ac
Volcomix <volcomix@gmail.com>
2017-04-17 17:36:17 +0200

    Fix redux store reload warning

--- containers/App.js
+++ containers/App.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import configureStore from '../store/configureStore'
 import GamesScreen from './GamesScreen'
 
-const store = configureStore()
-
 const App = () => (
-  <Provider store={store}>
-    <MuiThemeProvider>
-      <Router>
-        <Route exact path="/" component={GamesScreen} />
-      </Router>
-    </MuiThemeProvider>
-  </Provider>
+  <Router>
+    <Route exact path="/" component={GamesScreen} />
+  </Router>
 )
 
 export default App
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import { Provider } from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import moment from 'moment'
 import 'moment/locale/fr'
 
+import configureStore from './store/configureStore'
 import App from './containers/App'
 
 moment.locale('fr')
@@ -13,10 +16,16 @@ moment.locale('fr')
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+const store = configureStore()
+
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <Component />
+        </MuiThemeProvider>
+      </Provider>
     </AppContainer>,
     document.getElementById('root')
   )
